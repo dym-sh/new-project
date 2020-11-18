a8fb5c792ef02e3ae766a8aab240dca8572d9dc2
Dym Sohin <re@dym.sh>
2020-11-18 19:48:26 +0100

    __ 20201118@194826

---
+++ config.sh
@@ -0,0 +1,26 @@
+USER='dym-sh'
+
+PRODUCTION='dym.sh'
+
+REPOS='/Data/Lab'
+
+TEMPLATE="# $PROJECT
+
+> <DESCRIPTION>
+
+
+<a href='preview.png'><img height=100 border=2 align='right' alt='preview' src='preview.png'></a>
+## preview
+- [preview.html](preview.html)
+- https://codepen.io/$USER/pen/<TBD>/left/?editors=0100
+
+
+## mirrors
+- https://github.com/$USER/$PROJECT
+- https://gitlab.com/$USER/$PROJECT
+- https://$PRODUCTION/lab/$PROJECT
+- hyper://<TBD> /[?](https://beakerbrowser.com)
+
+
+## license
+[mit](license)"
